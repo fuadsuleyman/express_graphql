e30bb263d6e4b680e8c8abae2be674d2f388bbea
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-09-10 08:30:22 +0400

    add firts simple test query with graphql

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
+const { graphqlHTTP } = require('express-graphql');
+
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
 
 const app = express();
 
@@ -45,6 +49,12 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use('/graphql', 
+  graphqlHTTP({
+      schema: graphqlSchema,
+      rootValue: graphqlResolver
+    })
+  );
 
 app.use((error, req, res, next) => {
   console.log(error);
