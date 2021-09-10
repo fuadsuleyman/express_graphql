080c2dc500019f218f5ff63cd2a761c3d4ff3246
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-09-10 22:30:13 +0400

    more detalid error handling

---
+++ README.md
@@ -0,0 +1,10 @@
+# How to run
+
+- npm start 
+
+
+# How to test
+
+- Run server
+- Go to http://localhost:8080/graphql
+- Run query
--- app.js
+++ app.js
@@ -53,7 +53,16 @@ app.use('/graphql',
   graphqlHTTP({
       schema: graphqlSchema,
       rootValue: graphqlResolver,
-      graphiql: true
+      graphiql: true,
+      customFormatErrorFn(err){
+        if(!err.originalError){
+          return err;
+        }
+        const data = err.originalError.data;
+        const message = err.message || 'An error occureed!';
+        const code = err.originalError.code || 500;
+        return { message: message, status: code, data: data }
+      }
     })
   );
 
